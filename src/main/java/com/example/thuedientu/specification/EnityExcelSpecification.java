54222d1c4f1deb9aeec2ffc61ba62030d24bc8e6
DESKTOP-5SQT41E\KHANGVU <vuhuychuyen1992@gmail.com>
2025-04-07 04:56:53 +0700

    update

--- src/main/java/com/example/thuedientu/specification/EnityExcelSpecification.java
+++ src/main/java/com/example/thuedientu/specification/EnityExcelSpecification.java
@@ -1,35 +1,35 @@
-package com.example.thuedientu.specification;
-
-import com.example.thuedientu.model.EnityExcel;
-import com.example.thuedientu.model.EnityExcelSearchRequest;
-import org.springframework.data.jpa.domain.Specification;
-
-import jakarta.persistence.criteria.Predicate;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
-public class EnityExcelSpecification {
-
-    public static Specification<EnityExcel> withFilters(EnityExcelSearchRequest request) {
-        return (root, query, cb) -> {
-            List<Predicate> predicates = new ArrayList<>();
-
-            Field[] fields = EnityExcelSearchRequest.class.getDeclaredFields();
-            for (Field field : fields) {
-                try {
-                    field.setAccessible(true);
-                    Object value = field.get(request);
-                    if (value != null && value instanceof String str && !str.isBlank()) {
-                        predicates.add(cb.like(cb.lower(root.get(field.getName())), "%" + str.toLowerCase() + "%"));
-                    }
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return cb.and(predicates.toArray(new Predicate[0]));
-        };
-    }
-}
+//package com.example.thuedientu.specification;
+//
+//import com.example.thuedientu.model.EnityExcel;
+//import com.example.thuedientu.model.EnityExcelSearchRequest;
+//import org.springframework.data.jpa.domain.Specification;
+//
+//import jakarta.persistence.criteria.Predicate;
+//
+//import java.lang.reflect.Field;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class EnityExcelSpecification {
+//
+//    public static Specification<EnityExcel> withFilters(EnityExcelSearchRequest request) {
+//        return (root, query, cb) -> {
+//            List<Predicate> predicates = new ArrayList<>();
+//
+//            Field[] fields = EnityExcelSearchRequest.class.getDeclaredFields();
+//            for (Field field : fields) {
+//                try {
+//                    field.setAccessible(true);
+//                    Object value = field.get(request);
+//                    if (value != null && value instanceof String str && !str.isBlank()) {
+//                        predicates.add(cb.like(cb.lower(root.get(field.getName())), "%" + str.toLowerCase() + "%"));
+//                    }
+//                } catch (IllegalAccessException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//
+//            return cb.and(predicates.toArray(new Predicate[0]));
+//        };
+//    }
+//}
