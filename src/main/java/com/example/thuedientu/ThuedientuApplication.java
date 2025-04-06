5fbb41a7c08ac51bf02d4610ed9b306fdaf445b3
DESKTOP-5SQT41E\KHANGVU <vuhuychuyen1992@gmail.com>
2025-04-06 20:52:04 +0700

    update

--- src/main/java/com/example/thuedientu/ThuedientuApplication.java
+++ src/main/java/com/example/thuedientu/ThuedientuApplication.java
@@ -4,6 +4,7 @@ import org.apache.poi.util.IOUtils;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class ThuedientuApplication {
--- src/main/java/com/example/thuedientu/controller/ExcelImportController.java
+++ src/main/java/com/example/thuedientu/controller/ExcelImportController.java
@@ -1,11 +1,12 @@
 package com.example.thuedientu.controller;
 
-import com.example.thuedientu.service.ExcelImportService;
+import com.example.thuedientu.service.ExcellmportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 
 @RestController
@@ -14,16 +15,27 @@ import java.io.IOException;
 public class ExcelImportController {
 
     @Autowired
-    private ExcelImportService excelImportService;
+    private ExcellmportService excelImportService;
 
     // API để tải lên file Excel
     @PostMapping("/import")
     public ResponseEntity<String> importExcel(@RequestParam("file") MultipartFile file) {
+        File tempFile = null;
+
         try {
-            excelImportService.importExcel(file);  // Gọi dịch vụ để xử lý file
+            tempFile = File.createTempFile("myTemp", ".xlsx"); // or .txt depending on your use
+            System.out.println("Temp file created at: " + tempFile.getAbsolutePath());
+            file.transferTo(tempFile);
+
+            excelImportService.importAsync(tempFile);  // Now accessible here
             return ResponseEntity.ok("File Excel đã được nhập thành công.");
         } catch (IOException e) {
+            e.printStackTrace();
             return ResponseEntity.status(500).body("Lỗi khi xử lý file: " + e.getMessage());
+        } finally {
+            if (tempFile != null && tempFile.exists()) {
+                tempFile.delete(); // Clean up the temporary file
+            }
         }
     }
 }
