ec7c14e1bff6d153232bf1c11b8cab0641c154bf
DESKTOP-5SQT41E\KHANGVU <vuhuychuyen1992@gmail.com>
2025-04-08 06:38:22 +0700

    chay tam dc

--- src/main/java/com/example/thuedientu/ThuedientuApplication.java
+++ src/main/java/com/example/thuedientu/ThuedientuApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ThuedientuApplication {
 
 	public static void main(String[] args) {
