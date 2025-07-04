5a7e767a177350f8a63af680b94f1f32794a1937
DESKTOP-5SQT41E\KHANGVU <vuhuychuyen1992@gmail.com>
2025-07-04 22:30:30 +0700

    dfdsfsdfsdfsdfsdfsdfsdfs

---
+++ src/main/java/com/example/thuedientu/dto/TableNameDTO.java
@@ -0,0 +1,20 @@
+package com.example.thuedientu.dto;
+
+public class TableNameDTO {
+    private String tableName;
+
+    public TableNameDTO() {
+    }
+
+    public TableNameDTO(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+}
